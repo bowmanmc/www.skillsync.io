e011d24c3514f7f3ef81c8769c4028bf91befd41
Michael Bowman <bowmanmc@gmail.com>
2015-12-10 21:25:11 -0500

    rename to skillsync.io

--- Gulpfile.js
+++ Gulpfile.js
@@ -41,7 +41,7 @@ gulp.task('dist', function() {
     fs.appendFileSync(ver, '\nSkillSync.io - WWW');
     fs.appendFileSync(ver, '\n===================');
     fs.appendFileSync(ver, '\nName: ' + pkg.name);
-    fs.appendFileSync(ver, '\nURL: https://github.com/bowmanmc/ss-prelaunch');
+    fs.appendFileSync(ver, '\nURL: https://github.com/bowmanmc/skillsync.io');
     fs.appendFileSync(ver, '\nVersion: ' + pkg.version);
     fs.appendFileSync(ver, '\nGit Branch: ' + branch);
     fs.appendFileSync(ver, '\nGit Revision: ' + rev);
